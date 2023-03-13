eb49cc22042567ee6ab536fdb5b2b2cea7489873
TSAPLIN SERGEY <74894518+talatari@users.noreply.github.com>
2023-03-13 22:56:31 +0300

    Add draw ColorGenerator.
    Not work.

---
+++ Assets/HelixJumpSF/Scripts/Level/ColorGenerator.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorGenerator : MonoBehaviour
+{
+    [SerializeField] private Material _axis_mat;
+    [SerializeField] private Material _ball_mat;
+    [SerializeField] private Material _default_segment;
+    [SerializeField] private Material _finish_segment;
+    [SerializeField] private Material _trap_segment;
+
+    private Material _randomMaterial;
+    private List<Material> _materials;
+
+    private void Start()
+    {
+        //_materials.Add(_axisMaterial);
+        //_materials.Add(_ballMaterial);
+        //_materials.Add(_defaultMaterial);
+        //_materials.Add(_finishMaterial);
+        //_materials.Add(_trapMaterial);
+
+        //Debug.Log(_materials.Count);
+    }
+
+    public Material GetMaterial()
+    {
+        if (_materials.Count > 0)
+        {
+            int _randomIndex = Random.Range(0, _materials.Count);
+
+            _randomMaterial = _materials[_randomIndex];
+            _materials.RemoveAt(_randomIndex);
+
+            return _randomMaterial;
+        }
+        else
+        {
+            return _randomMaterial;
+        }
+    }
+}
--- Assets/HelixJumpSF/Scripts/Level/Segment.cs
+++ Assets/HelixJumpSF/Scripts/Level/Segment.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public enum SegmentType
@@ -11,22 +12,35 @@ public enum SegmentType
 [RequireComponent(typeof(MeshRenderer))]
 public class Segment : MonoBehaviour
 {
-    [SerializeField] private Material _trapMaterial;
+    [SerializeField] private Material _axisMaterial;
+    [SerializeField] private Material _ballMaterial;
+    [SerializeField] private Material _defaultMaterial;
     [SerializeField] private Material _finishMaterial;
+    [SerializeField] private Material _trapMaterial;
     [SerializeField] private SegmentType _type;
 
     public SegmentType Type => _type;
     private MeshRenderer _meshRenderer;
+    private ColorGenerator _colorGenerator;
+    private List<Material> _materials;
+    private Material _randomMaterial;
 
     private void Awake()
     {
         _meshRenderer = GetComponent<MeshRenderer>();
+
+        _materials.Add(_axisMaterial);
+        _materials.Add(_ballMaterial);
+        _materials.Add(_defaultMaterial);
+        _materials.Add(_finishMaterial);
+        _materials.Add(_trapMaterial);
     }
 
     public void SetTrap()
     {
         _meshRenderer.enabled = true;
         _meshRenderer.material = _trapMaterial;
+        //_meshRenderer.material = GetMaterial();
 
         _type = SegmentType.Trap;
     }
@@ -42,7 +56,25 @@ public class Segment : MonoBehaviour
     {
         _meshRenderer.enabled = true;
         _meshRenderer.material = _finishMaterial;
+        //_meshRenderer.material = GetMaterial();
 
         _type = SegmentType.Finish;
+    }
+
+    private Material GetMaterial()
+    {
+        if (_materials.Count > 0)
+        {
+            int _randomIndex = Random.Range(0, _materials.Count);
+
+            _randomMaterial = _materials[_randomIndex];
+            _materials.RemoveAt(_randomIndex);
+
+            return _randomMaterial;
+        }
+        else
+        {
+            return _randomMaterial;
+        }
     }
 }
